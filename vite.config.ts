b0de633c36d23a5fde21ed8a87913cf2a9dc8cfe
qinzhen <qz519189636@qq.com>
2024-09-18 14:22:46 +0800

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { name } from "./package.json"
 const genBaseUrl = (mode: string) => {
   if (mode == "production") {
     return `/${name}/`
+  } else if (mode == "test") {
+    return `/${name}/`
   }
   return "/"
 }
@@ -21,5 +23,15 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react()
     ],
+    build: {
+      rollupOptions: {
+        output: {
+          // 自定义输出目录结构
+          assetFileNames: '[name][extname]', // 将所有资源放在根目录
+          chunkFileNames: '[name].js', // 将所有 chunk 文件放在根目录
+          entryFileNames: '[name].js', // 将所有入口文件放在根目录
+        }
+      }
+    }
   }
 })
